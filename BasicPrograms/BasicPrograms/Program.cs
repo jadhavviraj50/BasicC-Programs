d71a8cd0af9db5d1b64f1b05a57ecf7c5074c49f
Viraj <jadhavviraj50@gmail.com>
2022-04-24 21:52:12 +0530

    [Viraj]-Added LearYear File

---
+++ BasicPrograms/BasicPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeapYear leap = new LeapYear();
+            leap.checkLeapYear();
+        }
+    }
+}
